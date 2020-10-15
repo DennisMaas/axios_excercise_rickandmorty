55213a0e1f0079aad972d71403e7d424254ccb33
DennisMaas <mail@dennismaas.de>
2020-10-15 21:00:38 +0200

    add image to background

--- src/components/CharacterInfo.js
+++ src/components/CharacterInfo.js
@@ -6,13 +6,13 @@ export default function CharacterInfo ({character}){
 
     return (
         <div>
-            <CharacterCard>
+
                     {name}
                     <img src={(image)} alt={""}/>
                         <p>Status: {status}</p>
                         <p>Gender: {gender}</p>
 
-            </CharacterCard>
+
         </div>
     );
 }
